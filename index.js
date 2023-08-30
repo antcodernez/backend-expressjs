cd1cbd43e4ba32c65586ad10a20996cf2f5eafc7
Antonio Jiménez <marshday5@gmail.com>
2023-08-29 19:16:49 -0600

    making the routing on my api

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Mi primer backend perras");
+// Creamos la app
+const express = require("express");
+const app = express(); // ejecutamos express como metodo dato que es un metodo constructor
+const port = 9222; // donde quiero que corra mi app
+
+// Usamos la app
+
+app.get('/', (req, res) => {
+    res.send("Hola mi server en express papus");
+});
+
+app.get("/nueva-ruta", (req, res) => {
+  res.send("Hola papus soy una nueva ruta en el servidor");
+})
+app.get("/productos", (req, res) => {
+  res.json({
+    name: "calcetines",
+    price: 1290
+  }); //Lo que mas vamos a enviar es un json porque voy a crear una api
+});
+app.get("/home", (req, res) => {
+  res.json({
+    text: "Soy un index",
+    oferts: [{
+        name: "Calcetines",
+        price: 120
+      },
+      {
+        name: "pelos",
+        price: 1210
+      }]
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Ya estoy funcionando master en el puerto ${port}`);
+});
