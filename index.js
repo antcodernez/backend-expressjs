072686fd825e0fbd02766914f60dfe8fe4374d72
Antonio Jiménez <marshday5@gmail.com>
2023-08-30 11:34:55 -0600

    Implenting the library faker and making query parameters on my endpoints

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // Creamos la app
 const express = require("express");
+const {faker} = require("@faker-js/faker"); //Libreria para simular datos nueva
+
 const app = express(); // ejecutamos express como metodo dato que es un metodo constructor
 const port = 9222; // donde quiero que corra mi app
 
@@ -14,94 +16,29 @@ app.get("/nueva-ruta", (req, res) => {
 })
 
 //Lo que mas vamos a enviar es un json porque voy a crear una api
-//endpoint de productos
+//endpoint de productos, ahora usando la librearia faker estamos creando un query parameter de size para traer productos, el default es 10, pero puedo traer n cantidad
 app.get("/products", (req, res) => {
-  res.json([
+
+  const products = [];
+  const {size} = req.query;
+  const limit = size || 10;
+
+  for(let i = 0; i < limit; i++)
     {
-      id: 1,
-      name: "underwear",
-      products: [{
-        id: 1,
-        name: "calcetines",
-        price: 120,
-        details: "It's very nice to used"
-      },
-      {
-        id: 2,
-        name: "t-shirt",
-        price: 300,
-        details: "You will be awesome with this"
-      },
-      {
-        id: 3,
-        name: "underwear",
-        price: 3001,
-        details: "You will be too hot with this"
-      },
-      {
-        id: 4,
-        name: "Dress",
-        price: 524,
-        details: "You will be very sexy"
-      }]
-    },
-    {
-      id: 2,
-      name: "girls",
-      products: [{
-        id: 1,
-        name: "calcetines",
-        price: 120,
-        details: "It's very nice to used"
-      },
-      {
-        id: 2,
-        name: "t-shirt",
-        price: 300,
-        details: "You will be awesome with this"
-      },
-      {
-        id: 3,
-        name: "underwear",
-        price: 3001,
-        details: "You will be too hot with this"
-      },
-      {
-        id: 4,
-        name: "Dress",
-        price: 524,
-        details: "You will be very sexy"
-      }]
-    },
-    {
-      id: 2,
-      name: "boys",
-      products: [{
-        id: 1,
-        name: "calcetines",
-        price: 120,
-        details: "It's very nice to used"
-      },
-      {
-        id: 2,
-        name: "t-shirt",
-        price: 300,
-        details: "You will be awesome with this"
-      },
-      {
-        id: 3,
-        name: "underwear",
-        price: 3001,
-        details: "You will be too hot with this"
-      },
-      {
-        id: 4,
-        name: "Dress",
-        price: 524,
-        details: "You will be very sexy"
-      }]
+      products.push(
+        {
+          id: i,
+          name: faker.commerce.productName(),
+          price: parseInt(faker.commerce.price()),
+          image: faker.image.url(),
+        });
     }
-  ]);
+  res.json(products);
+});
+
+//Todo lo que es especifico debe ir antes de todo lo que es dinamico, en este caso los endpoints de filter y :id no chocan porque antes esta filter, que id pero si filter lo bajaramos antes de /products/:id me retornaria filter como un id y chocarian los endpoints de la api
+app.get("/products/filter", (red, res) => {
+    res.send("Yo soy un filter");
 });
 
 
@@ -109,7 +46,6 @@ app.get("/products", (req, res) => {
 //Al momento de crearlo, los dos puntitos : significan que es un parametro
 app.get("/products/:id", (req, res) => {
     const { id }= req.params; //recogemos el id que me estan enviando y lo vamos a enviar el la respuesta, eso viene el request(req)
-
     res.json({
       id,
       name: "t-shirt",
@@ -126,6 +62,21 @@ app.get("/categories/:categoryId/products/:productId", (req, res) => {
         productId
       });
   });
+//Ejemplo de consultas de parametros query
+app.get("/users", (req, res) => {
+  const { limit, offset } = req.query; // asi voy a tener obtener los parametros que vengas desde query
+  if(limit || offset)
+    {
+      res.json({
+        limit,
+        offset
+      })
+    }
+  else
+    {
+      res.send("No hay parametros");
+    }
+});
 
 app.get("/home", (req, res) => {
   res.json({
