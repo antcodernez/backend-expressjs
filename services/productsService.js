3ffe86f746b011faa19d1ca3ba23604a76fbed0f
Antonio Jiménez <marshday5@gmail.com>
2023-09-02 18:15:21 -0600

    first steps implementing clean architecture

---
+++ services/productsService.js
@@ -0,0 +1,37 @@
+
+//Vamos a definir la lógica de las interacciones a nivel transaccional que van a tener mi datos
+//Manejo transaccional hacia un producto
+
+class ProductService
+  {
+    constructor()
+      {
+        this.products = [];
+      }
+      generate()
+        {
+
+        }
+      create()
+        {
+
+        }
+      find()
+        {
+
+        }
+      findOne()
+        {
+
+        }
+      update()
+        {
+
+        }
+      delete()
+        {
+
+        }
+  }
+
+module.exports = ProductService;
